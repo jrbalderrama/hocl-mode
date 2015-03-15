02712a4bcd3f233b408deb1f3140ba37266db516
jrbalderrama <javier.rojas-balderrama@inria.fr>
2015-03-15 21:22:55 +0100

    Add doc strings

--- hocl-mode.el
+++ hocl-mode.el
@@ -1,6 +1,53 @@
 ;;; hocl-mode.el --- HOCL mode -*- lexical-binding: t -*-
 ;;; Commentary:
+;;
+;;  Simple mode for HOCL files.
+;;  Site: https://github.com/jrbalderrama/hocl-mode
+;;
 ;;; Code:
+
+(defconst hocl-types
+  '("String" "int" "boolean" "float" "Object")
+  "HOCL reserved words for data types.")
+
+;; (defconst hocl-constants
+;;   '("SRC" "IN" "PAR" "RES" "SRV" "DST"))
+
+(defconst hocl-keywords
+  '("let" "replace" "replace-one" "by" "if" "in")
+  "HOCL reserved words for language keywords.")
+
+(defvar hocl-buildin-regexp
+  "<\\|>\\|?\\|\\[\\|\\]"
+  "HOCL build-in symbols.
+Normally non [:alnum:] characters.")
+
+(defvar hocl-function-regexp
+  "\\_<[a-z]\\{2\\}_[[:alnum:]_]+\\_>"
+  "HOCL function name pattern.
+The accepted pattern is associated to the string xx_y[y]+ where
+'x' is an alphabet char and 'y' is an alphanumeric char.  For
+example: 'gw_pass'.")
+
+(defvar hocl-type-regexp
+  (regexp-opt hocl-types 'words)
+  "HOCL variable type pattern.")
+
+;; (defvar hocl-constant-regexp
+;;   (regexp-opt hocl-constants 'words)
+;;   "HOCL constant pattern.")
+
+;; (defvar hocl-variable-regexp
+;;   "\\(\\?\\)?[a-z]_[[:alnum:]]\\{1,3\\}"
+;;   "HOCL variable name pattern.
+;; The suggested pattern is associated to the string format
+;; 'x_y[yy]' where 'x' is an alphabet char and 'y' is a alphanumeric
+;; char.  For example: 'w_src'.")
+
+(defvar hocl-keyword-regexp
+  ;; "\\_<\\(let\\|replace\\([\\-]one\\)?\\|by\\|if\\|in\\)\\_>"
+  (regexp-opt hocl-keywords 'symbols)
+  "HOCL keywords pattern.")
 
 (defvar hocl-syntax-table
   (let ((hocl-table (make-syntax-table)))
@@ -9,51 +56,32 @@
     hocl-table)
   "Syntax table for `hocl-mode'.")
 
+(defvar hocl-font-lock-keywords
+  `((,hocl-buildin-regexp . font-lock-builtin-face)
+    (,hocl-function-regexp . font-lock-function-name-face)
+    (,hocl-type-regexp . font-lock-type-face)
+    ;; (,hocl-constant-regexp . font-lock-constant-face)
+    ;; (,hocl-variable-regexp . font-lock-variable-name-face)
+    (,hocl-keyword-regexp . font-lock-keyword-face)))
+
 (defun hocl-comment-dwim (arg)
   "Comment or uncomment current line or region (ARG) in a smart way.
-For detail, see `comment-dwim'."
+For details see `comment-dwim'."
   (interactive "*P")
   (require 'newcomment)
   (let ((comment-start "//")
         (comment-end ""))
     (comment-dwim arg)))
 
-(defconst hocl-keywords
-  '("let" "replace" "replace-one" "by" "if" "in"))
-
-(defconst hocl-types
-  '("String" "int" "boolean" "float" "Object"))
-
-;; (defconst hocl-constants
-;;   '("SRC" "IN" "PAR" "RES" "SRV" "DST"))
-
-(defvar hocl-keyword-regexp
-  (regexp-opt hocl-keywords 'symbols))
-
-(defvar hocl-type-regexp
-  (regexp-opt hocl-types 'words))
-
-;; (defvar hocl-constant-regexp
-;;   (regexp-opt hocl-constants 'words))
-
-(defvar hocl-font-lock-keywords
-  `(("<\\|>\\|?\\|\\[\\|\\]" . font-lock-builtin-face)
-    ("\\_<[a-z]\\{2\\}_[[:alnum:]_]+\\_>" . font-lock-function-name-face)
-    (,hocl-type-regexp . font-lock-type-face)
-    ;; (,hocl-constant-regexp . font-lock-constant-face)
-    ;; ("\\(\\?\\)?[a-z]_[[:alnum:]]\\{1,3\\}". font-lock-variable-name-face)
-    (,hocl-keyword-regexp . font-lock-keyword-face)
-    ;; ("\\_<\\(let\\|replace\\([\\-]one\\)?\\|by\\|if\\|in\\)\\_>" . font-lock-keyword-face)
-    ))
-
-(add-to-list 'auto-mode-alist '("\\.hocl\\'" . hocl-mode))
-
-(define-derived-mode hocl-mode fundamental-mode "HOCL-mode" nil
+(define-derived-mode hocl-mode fundamental-mode "HOCL-mode"
+  "Simple mode to highlight HOCL keywords and support comments."
   :syntax-table hocl-syntax-table
 
-  (setq-local font-lock-keywords-only t)
   (setq font-lock-defaults '((hocl-font-lock-keywords)))
   (define-key hocl-mode-map [remap comment-dwim] 'hocl-comment-dwim))
+
+;; associate files with 'hocl' extension with this mode
+(add-to-list 'auto-mode-alist '("\\.hocl\\'" . hocl-mode))
 
 (provide 'hocl-mode)
 
